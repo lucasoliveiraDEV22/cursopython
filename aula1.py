a215ad5508b6d38445e93d200cf20bfdc3a7fb21
Lucas Oliveira <116319203+lucasoliveiraDEV22@users.noreply.github.com>
2023-09-25 18:12:13 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,14 @@
+# noteOne = float(input('Digite a primeira nota: '))
+# noteTwo = float(input('Digite a segunda nota: '))
+# noteThree = float(input('Digite a terceira nota: '))
+
+# media = (noteOne + noteTwo + noteThree) / 3
+
+
+num = 3
+num = num + 2
+num = num*2
+
+print(num)
+
+# print("Fala devs! Tudo bem?")
